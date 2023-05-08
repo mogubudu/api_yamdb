f58da20a475f6b08d8240b8937b6dad06ec450a8
Vladislav Pipkin <pipkin.vladislav@yandex.ru>
2023-05-08 22:43:15 +0300

    Add blank custom user model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    bio = models.TextField(
+        verbose_name='Биография',
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
