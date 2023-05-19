de0b1dd6610164017717a313f6ddfcfbb4d4ad24
Vladislav Pipkin <pipkin.vladislav@yandex.ru>
2023-05-19 20:17:52 +0300

    Add import from cdv

--- api_yamdb/reviews/management/commands/import_data.py
+++ api_yamdb/reviews/management/commands/import_data.py
@@ -1,40 +1,43 @@
 import csv
-import os
-from django.core.management.base import BaseCommand
-from models import Category, Genre, Title, Review, User, Comment
 
-# Словарь соответствия моделей файлам CSV
-models = {
-    'category.csv': Category,
-    'genre.csv': Genre,
-    # 'genre_title.csv': Title, #не уверен в данном виде связи
-    'titles.csv': Title,
-    'review.csv': Review,
-    'users.csv': User,
-    'comments.csv': Comment
+from django.core.management import BaseCommand
+from django.db import IntegrityError
+
+from api_yamdb.settings import BASE_DIR
+from reviews.models import Category, Comment, Genre, Review, Title, User
+
+TABLES = {
+    User: 'users.csv',
+    Category: 'category.csv',
+    Genre: 'genre.csv',
+    Title: 'titles.csv',
+    Review: 'review.csv',
+    Comment: 'comments.csv',
 }
 
 
 class Command(BaseCommand):
-    help = 'Import data from csv file'
 
-    def add_arguments(self, parser):
-        parser.add_argument('files', nargs='+', type=str)
-        
-    def handle(self, *args, **options):
-        files = options['files']
-
-        for file_name in files:
-            file_path = os.path.join('static/data', file_name)
-            if file_name not in models.keys():
-                self.stderr.write('File not supported: %s' % file_name)
-                continue
-            model = models[file_name]
-            with open(file_path) as f:
-                reader = csv.reader(f)
-                next(reader)
-                for row in reader:
-                    instance = model()
-                    for i, field in enumerate(model._meta.fields):
-                        setattr(instance, field.name, row[i])
-                    instance.save()
+    def handle(self, *args, **kwargs):
+        for model, csv_f in TABLES.items():
+            with open(
+                f'{BASE_DIR}/static/data/{csv_f}',
+                'r',
+                encoding='utf-8'
+            ) as csv_file:
+                reader = csv.DictReader(csv_file)
+                try:
+                    model.objects.bulk_create(model(**data) for data in reader)
+                except ValueError:
+                    self.stdout.write(
+                        self.style.ERROR(
+                            'Переименуйте, пожалуйста, значение category, '
+                            'author добавив к ним _id.'
+                            'Должно получиться: category_id, author_id'))
+                except IntegrityError:
+                    self.stdout.write(
+                        self.style.ERROR(
+                            'Ошибка базы данных, возможно вы уже добавили '
+                            'значения из этих файлов.'
+                        ))
+        self.stdout.write(self.style.SUCCESS('Данные успешно загружены'))
