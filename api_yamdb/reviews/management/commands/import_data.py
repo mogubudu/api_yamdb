b53ed54fed39ddd6668f7206fafece86332f25e6
Vladislav Pipkin <pipkin.vladislav@yandex.ru>
2023-05-20 02:02:40 +0300

    Edited import

--- api_yamdb/reviews/management/commands/import_data.py
+++ api_yamdb/reviews/management/commands/import_data.py
@@ -1,12 +1,16 @@
 import csv
+import os
 
 from django.core.management import BaseCommand
-from django.db import IntegrityError
 
 from api_yamdb.settings import BASE_DIR
-from reviews.models import Category, Comment, Genre, TitleGenre, Review, Title, User
+from reviews.models import (Category, Comment,
+                            Genre, TitleGenre,
+                            Review, Title, User)
 
-TABLES = {
+FILE_DIR = f'{BASE_DIR}/static/data/'
+
+models_table = {
     User: 'users.csv',
     Category: 'category.csv',
     Genre: 'genre.csv',
@@ -18,27 +22,37 @@ TABLES = {
 
 
 class Command(BaseCommand):
-
     def handle(self, *args, **kwargs):
-        for model, csv_f in TABLES.items():
-            with open(
-                f'{BASE_DIR}/static/data/{csv_f}',
-                'r',
-                encoding='utf-8'
-            ) as csv_file:
+        for model, file_name in models_table.items():
+            file_path = f'{FILE_DIR}/{file_name}'
+
+            if not os.path.exists(file_path):
+                self.stdout.write(
+                    self.style.ERROR(
+                        f'{file_name} отсутствует среди файлов'
+                    )
+                )
+                continue
+
+            with open(file_path, 'r+', encoding='utf-8') as csv_file:
                 reader = csv.DictReader(csv_file)
+                if 'category' in reader.fieldnames:
+                    replace_index = reader.fieldnames.index('category')
+                    reader.fieldnames[replace_index] = 'category_id'
+                if 'author' in reader.fieldnames:
+                    replace_index = reader.fieldnames.index('author')
+                    reader.fieldnames[replace_index] = 'author_id'
                 try:
                     model.objects.bulk_create(model(**data) for data in reader)
-                except ValueError:
+                    self.stdout.write(
+                        self.style.SUCCESS(
+                            f'Данные из {file_name} успешно загружены'
+                        )
+                    )
+                except Exception as error:
                     self.stdout.write(
                         self.style.ERROR(
-                            'Переименуйте, пожалуйста, значение category, '
-                            'author добавив к ним _id.'
-                            'Должно получиться: category_id, author_id'))
-                except IntegrityError:
-                    self.stdout.write(
-                        self.style.ERROR(
-                            'Ошибка базы данных, возможно вы уже добавили '
-                            'значения из этих файлов.'
-                        ))
-        self.stdout.write(self.style.SUCCESS('Данные успешно загружены'))
+                            f'Данные из {file_name} '
+                            f'загрузить не удалось: {error}'
+                        )
+                    )
