3a6a7c9d4fcfc715126995c658200acac2d9b2f4
Максим Кузьмин <ivanKatamaran11@gmail.com>
2023-05-22 18:30:28 +0300

    bred

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from reviews.validators import validate_year
 
 from api_yamdb.settings import MIN_SCORE, MAX_SCORE
 
